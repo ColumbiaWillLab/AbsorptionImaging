0362e279652cf2ab9153c4f64841b373b14c90d8
David Grabovsky <djg2182@columbia.edu>
2019-05-02 14:47:33 -0400

    Added all files

---
+++ Helper.py
@@ -0,0 +1,303 @@
+import time
+
+import math
+import numpy as np
+
+from scipy.optimize import curve_fit
+from scipy.ndimage import filters
+from lmfit import minimize, Parameters
+
+import matplotlib.pyplot as plt
+import matplotlib.path as mplPath
+import matplotlib.gridspec as gridspec
+
+class MyExcept(Exception):
+    """
+    Raise this when 3 new files have been detected.
+    """
+
+# Preliminary data processing
+def subtraction(data, beam, dark, kernel):
+    """
+    INPUT: 3 image arrays (background, laser beam, absorption image).
+    PROCESSING: subtract background from both data and beam arrays; divide
+    absorption data by beam background to get the transmission t^2.
+    Then apply a Gaussian kernel filter to smooth out remaining noise.
+    OUTPUT: numpy array containing transmission (0 < t^2 < 1) values.
+    """
+    print("Performing background subtraction")
+    background = beam.astype(int) - dark.astype(int)
+    image = data.astype(int) - dark.astype(int)
+    transmission = image.astype(float) / background.astype(float)
+    time.sleep(.01)
+    transmission[background <= 3] = 1
+    time.sleep(.01)
+
+    print("Applying Gaussian filter of size " + str(kernel))
+    transmission = filters.gaussian_filter(transmission, kernel)
+    return transmission
+
+# Auxiliary analysis functions
+def params2list(parameters):
+    # Converts a Parameters object (parameters) to a list (array)
+
+    fancy = parameters.valuesdict().items()
+    values = []
+    for entry in fancy:
+        values.append(entry[1])
+    return values
+def list2params(value):
+    # Converts a list (val) to a Parameters object (params) 
+
+    params = Parameters()
+    params.add('A', value = value[0])
+    params.add('x0', value = value[1])
+    params.add('y0', value = value[2])
+    params.add('sigma_x', value = value[3])
+    params.add('sigma_y', value = value[4])
+    params.add('theta', value = value[5])
+    params.add('z0', value = value[6])
+    return params
+def peak_find(data, f):
+    """
+    INPUT: data contains the array searched for a peak.
+    f is the factor by which to reduce the resolution.
+    OUTPUT: the indices and value (x0, y0, val) of the peak in data.
+    """
+    # use a peak-finding algorithm like scipy.peakfind.cwt???
+
+    print("Finding a transmission peak")
+    flat_data = data.ravel()
+    minimum = np.argmin(flat_data)
+    shape = (len(data), len(data[0]))
+    (y0_ind, x0_ind) = np.unravel_index(minimum, shape)
+    val = data[y0_ind][x0_ind]
+    (y0, x0) = (x0_ind * f, y0_ind * f)
+    print(x0,y0,val)
+    return (x0, y0, val)
+def de_enhance(data, f):
+    """
+    INPUT: data contains the high-res image to be blockified;
+    f is the factor by which to reduce the resolution.
+    OUTPUT: a de-enhanced array containing the image data.
+    """
+
+    print("De-enhancing by a factor of " + str(f))
+    coarse = []
+    w = len(data[0])/f
+    h = len(data)/f
+
+    # skip every f pixels and append to a smaller array
+    for i in range(h):
+        row = []
+        for j in range(w):
+            row.append(data[i*f][j*f])
+        coarse.append(row)
+    return np.array(coarse)
+def zoom_in(data, r):
+    """
+    INPUT: data is the large array, zoomed into user-defined ROI 
+    given by r = (xmin, ymin, w, h).
+    OUTPUT: smaller array zooming in on a Gaussian feature.
+    """
+    
+    # define bounds of zoomed array
+    xmin = r[0]
+    ymin = r[1]
+    xmax = r[0] + r[2]
+    ymax = r[1] + r[3]
+
+    # build the zoomed-in array
+    zoomed = []
+    for row in data[ymin:ymax]:
+        new_row = row[xmin:xmax]
+        zoomed.append(new_row)
+
+    print("Zooming in: (" +str(r[2])+" p) x ("+str(r[3])+" p)")
+    return zoomed
+
+# Gaussian fitting procedure
+def gaussian(params, x,y):
+    """
+    INPUT: params = [A, x0, y0, sigma_x, sigma_y, theta] contains the
+    amplitude, center point, standard deviations, and angle of the blob.
+    (x,y) is the meshgrid defining the function domain.
+    OUTPUT: values of the Gaussian function f(x).
+    """
+
+    # Unpack parameters
+    values = params2list(params)
+    (A, x0, y0, sx, sy, th, z0) = values
+
+    # Define constants (see Wikipedia)
+    a = (math.cos(th)**2)/(sx**2) + (math.sin(th)**2)/(sy**2)
+    b = -(np.sin(2*th))/(2*sx**2) + (np.sin(2*th))/(2*sy**2)
+    c = (np.sin(th)**2)/(sx**2) + (np.cos(th)**2)/(sy**2)
+
+    # Create the Gaussian function
+    quadratic = a*(x-x0)**2 + 2*b*(x-x0)*(y-y0) + c*(y-y0)**2
+    return A * np.exp(-0.5*quadratic) + z0
+def residual(params, x,y, data):
+    """
+    INPUT: params = [A, x0, y0, sigma_x, sigma_y, theta, z0].
+    (x,y) defines the Gaussian's domain; data is the data to be fitted.
+    OUTPUT: flattened error signal array, (1 - gaussian) - data.
+    """
+
+    model = 1 - gaussian(params, x,y)
+    return (model - data).flatten()
+def minimizer(residual, params, x,y, data):
+    """
+    INPUT: residual is the function to be minimized;
+    params contains the parameters to be optimized;
+    data contains the data to be fitted to the model.
+    OUTPUT: best-fit parameters and set of data based on them.
+    """
+
+    # Perform the minimization
+    out = minimize(residual, params, xtol = 1e-3, args = (x,y, data))
+    best = params2list(out.params)
+    param0 = list2params(best)
+
+    # Generate best-fit data
+    fit_data = 1 - gaussian(param0, x,y)
+    return fit_data, best
+def iterfit(residual, guess, x,y, width,height, data, num):
+    """
+    INPUT: residual is the function (model - data) to be minimized;
+    guess contains the starting parameters for fitting by minimizer;
+    data is the data to be fitted; num = # of fitting iterations.
+    OUTPUT: best-fit parameters and a set of data based on them.
+    """
+
+    print("Performing a 2D Gaussian fit")
+    # Set initial guess
+    p = guess
+    print("Guess:    " + str(np.round(p,2)))
+
+    for i in range(num): # iterate the fit [num] times
+        # Load parameter object with values from most recent fit
+        params = Parameters()
+        params.add('A', value = p[0], min = 0, max = 2)
+        params.add('x0', value = p[1], min = 0, max = width)
+        params.add('y0', value = p[2], min = 0, max = height)
+        params.add('sigma_x', value = p[3], min = 1, max = width)
+        params.add('sigma_y', value = p[4], min = 1, max = height)
+        params.add('theta', value = p[5], min = -math.pi/4, max = math.pi/4)
+        params.add('z0', value = p[6], min = -2, max = 2)
+
+        # Do the minimization; redefine the initial guess
+        fit_data, p = minimizer(residual, params, x,y, data)
+
+    print("Best fit: " + str(np.round(p,2)))
+    return fit_data, p
+
+# 1D analysis and fitting
+def lines(x, params):
+    """
+    INPUT: lists of domain values and the best-fit Gaussian parameters.
+    OUTPUT: two lists of coordinates (x_hor, y_hor) and (x_ver, y_ver)
+    lying along the horizontal & vertical axes of the Gaussian.
+    """
+
+    # Unpack parameters
+    (x0, y0) = (params[1], params[2])
+    theta = params[5]
+    theta = 0
+
+    # point-slope and y = mx + b
+    m = -np.tan(theta)
+    b = -m*x0 + y0
+    y_hor = m*x + b
+    x_hor = x
+
+    # rotate the lines
+    x_ver = -(y_hor - y0) + x0
+    y_ver = (x_hor - x0) + y0
+
+    return x_hor, y_hor, x_ver, y_ver
+def collect_data(data, x_val, y_val, axis):
+    """
+    INPUT: data is the array to be mined for data; x_val and y_val
+    are (x,y) locations in pixels where data is to be retreived;
+    axis is x_val or y_val, and is the plotting axis for data.
+    OUTPUT: a list of coordinates (either x or y) and corresponding
+    pixel values along those coordinates.
+    """
+
+    # approximate pixel locations of sampled points
+    x_pix = np.round(x_val).astype(int)
+    y_pix = np.round(y_val).astype(int)
+    width = len(data[0])
+    height = len(data)
+
+    output_axis = []
+    output_data = []
+    if axis == 'x':
+        ax = x_pix
+    elif axis == 'y':
+        ax = y_pix
+
+    for i in range(len(ax)): # pick pixel locations in valid range
+        if (0 <= y_pix[i] < height) and (0 <= x_pix[i] < width):
+            coord = ax[i]
+            value = data[y_pix[i]][x_pix[i]]
+            output_axis.append(coord)
+            output_data.append(value)
+    return np.array(output_axis), np.array(output_data)
+def gaussian_1d(params, x):
+    """
+    INPUT: x0, sigma, and z0 are parameters of the Gaussian f(x);
+    x is the list containing its domain.
+    OUTPUT: a list containing values of the Gaussian 1 - f(x).
+    """
+
+    # Unpack parameters
+    values = params2list(params)
+    (A, x0, sigma, z0) = values
+
+    # Create the Gaussian function
+    quadratic = (x-x0)**2 / sigma**2
+    return A * np.exp(-0.5*quadratic) + z0
+def residual_1d(params, x, data):
+    """
+    INPUT: params = [A, x0, sigma, z0] are the Gaussian's parameters;
+    x is the Gaussian's domain; data is the data to be fitted.
+    OUTPUT: error signal list, (1 - gaussian_1d) - data.
+    """
+
+    model = 1 - gaussian_1d(params, x)
+    return model - data
+def fit_1d(residual, guess, x, data):
+    """
+    INPUT: residual is the function (model - data) to be minimized;
+    guess contains the starting parameters for the fit; x is the domain.
+    OUTPUT: best-fit parameters and a set of data based on them.
+    """
+
+    print("Performing a 1D Gaussian fit")
+    p = guess
+    print("Guess:    " + str(np.round(p,2)))
+    # Set initial guess
+    params = Parameters()
+    params.add('A', value = p[0], min = 0, max = 2)
+    params.add('x0', value = p[1], min = p[1] - 100, max = p[1] + 100)
+    params.add('sigma', value = p[2], min = 1, max = len(data))
+    params.add('z0', value = p[3], min = -2, max = 2)
+
+    # Do the minimization; redefine the initial guess
+    out = minimize(residual, params, args = (x, data))
+    best = params2list(out.params)
+    
+    # Convert best to a Parameters() object
+    # This is shameful programming...
+    param0 = Parameters()
+    param0.add('A', value = best[0])
+    param0.add('x0', value = best[1])
+    param0.add('sigma', value = best[2])
+    param0.add('z0', value = best[3])
+
+    # Generate best-fit data
+    print("Best fit: " + str(np.round(best, 2)))
+    fit_data = 1 - gaussian_1d(param0, x)
+    return fit_data, best
---
+++ Imaging.py
@@ -0,0 +1,420 @@
+##################################################
+######## 0: CLASSES, FUNCTIONS, LIBRARIES ########
+##################################################
+
+print("0: SET UP LIBRARIES/FUNCTIONS")
+print("------------------------------")
+
+import time
+start = time.clock()
+
+import os
+import shutil
+import imageio
+import cv2
+
+import math
+import numpy as np
+np.set_printoptions(suppress=True) # suppress scientific notation
+
+from scipy.optimize import curve_fit
+from lmfit import minimize, Parameters
+
+import matplotlib.pyplot as plt
+import matplotlib.path as mplPath
+import matplotlib.gridspec as gridspec
+
+# all function definitions and classes
+import Helper as hp
+
+# mode = 'manual' or 'automatic'
+mode = 'manual'
+
+stop = time.clock()
+print("Initializing took " + str(round(stop - start, 2)) + " seconds")
+print(" ")
+
+##################################################
+######### 1: READ FILES AND CREATE DATA ##########
+##################################################
+
+shot = 1
+
+# main data analysis loop; runs until ctrl+c or ctrl+. interrupts
+while True:
+    initial = time.clock()
+    print("SHOT " + str(shot))
+    print(" ")
+
+    print("1: READ FILES AND CREATE DATA")
+    print("-------------------------------")
+    start = time.clock()
+    
+    count = 0
+    despacito2 = []
+    path_to_watch = "."
+    before = dict([(f, None) for f in os.listdir(path_to_watch)])
+    print("Watching for new files...")
+    
+    try: # main file monitoring loop - counts to 3
+        while True:
+            after = dict ([(f, None) for f in os.listdir (path_to_watch)])
+            added = [f for f in after if not f in before]
+            removed = [f for f in before if not f in after]
+            
+            if added: # append filename to despacito2; move counter up
+                print "Added: ", ", ".join(added)
+                despacito2.extend(added)
+                for filename in added:
+                    count += 1
+                if count >= 3:
+                    raise hp.MyExcept # found 3 images; exit the loop
+            if removed: # move counter down or keep it at 0
+                print "Removed: ", ", ".join(removed)
+                if despacito2 != []: # files were deleted mid-shot
+                    for filename in removed:
+                        del despacito2[-1]
+                        count -= 1
+                else: # some other file was deleted: do nothing
+                    pass
+            
+            before = after
+    except hp.MyExcept:
+        plt.close()
+    despacito2.sort() # make sure the images are in order
+    
+    # read images into large arrays of pixel values    
+    print("Writing image data into arrays")
+    print(despacito2)
+    data = imageio.imread(despacito2[0])
+    beam = imageio.imread(despacito2[1])
+    dark = imageio.imread(despacito2[2])
+    width = len(data[0])
+    height = len(data)
+    
+    # save raw images in a new folder
+    garbage_path = '../Raw Data/'
+    now = time.strftime("%Y%m%d-%H%M%S")
+    pic_num = 1
+    
+    for meme in despacito2:
+        name = "Raw_%s_%s.bmp" % (now, str(pic_num))
+        os.rename(meme, name)
+        shutil.copy2(name, garbage_path)
+        pic_num += 1
+        os.remove(name)
+
+    # create a meshgrid: each pixel is (3.75 um) x (3.75 um); images
+    # have resolution (964 p) x (1292 p) --> (3.615 mm) x (4.845 mm).
+    pixelsize = 3.75e-3 # 3.75 um, reported in mm.
+    x = np.linspace(0, width, width)
+    y = np.linspace(0, height, height)
+    (x,y) = np.meshgrid(x,y)
+    pixels = [0, pixelsize * width, pixelsize * height, 0]
+
+    # create fake data for laser & atom sample; do background subtraction
+    kernel = 2
+    noise = 1   
+    # data, beam, dark = fake_data(laser, atoms, noise)
+    transmission = hp.subtraction(data, beam, dark, kernel)
+
+    """
+    # show transmission plot - debugging purposes only
+    plt.close()
+    plt.figure(1)
+    plt.imshow(transmission, cmap = 'inferno', extent = pixels)
+    plt.colorbar()
+    plt.draw() # display now; keep computing
+    """
+
+    stop = time.clock()
+    print("Writing data took " + str(round(stop - start, 2)) + " seconds")
+
+    print(" ")
+
+    ##################################################
+    ######### 2: GAUSSIAN FITTING (2D IMAGE) #########
+    ##################################################
+
+    print("2: GAUSSIAN FITTING (2D IMAGE)")
+    print("-------------------------------")
+    start = time.clock()
+    
+    print("Mode: " + mode)
+    # gaussian parameters: p = [A, x0, y0, sigma_x, sigma_y, theta, z0]
+
+    # compute parameters automatically
+    if mode == 'automatic':
+        f = 5
+        
+        # coarsen the image; create a coarse meshgrid for plotting
+        coarse = hp.de_enhance(transmission, f)
+        x_c = np.linspace(f, len(coarse[0])*f, len(coarse[0]))
+        y_c = np.linspace(f, len(coarse) * f, len(coarse))
+        (x_c, y_c) = np.meshgrid(x_c, y_c)
+
+        # take an "intelligent" guess and run the coarse fit
+        (y0, x0, peak) = hp.peak_find(coarse, f) # guess an initial center point
+        (amp, z0) = (transmission[0][0] - peak, 1 - transmission[0][0])
+        guess = [amp, x0, y0, 200, 200, 0, z0]
+        coarse_fit, best = hp.iterfit(hp.residual,guess,x_c,y_c,width,height,coarse,1)
+
+        # compute the relative error from the coarse fit
+        error = (coarse - coarse_fit) / coarse
+        area = (width * height) / (f**2)
+        int_error = (np.sum((error)**2) / area) * 1000
+        print("Integrated error: " + str(round(int_error, 2)))
+        
+    # guess parameters based on user input
+    elif mode == 'manual':
+        f = 3
+        
+        # allow the user to select a region of interest
+        r = cv2.selectROI(transmission)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+        # zoom in, coarsen, and create a coarse meshgrid
+        zoomed = hp.zoom_in(transmission, r)
+        coarse = hp.de_enhance(zoomed, f)
+        x_c = np.linspace(f, len(coarse[0])*f, len(coarse[0]))
+        y_c = np.linspace(f, len(coarse) * f, len(coarse))
+        (x_c, y_c) = np.meshgrid(x_c, y_c)
+        
+        # take an intelligent guess at fit parameters
+        (y0, x0, peak) = hp.peak_find(coarse, f)
+        (amp, z0) = (transmission[0][0] - peak, 1 - transmission[0][0])
+        sigma_x = 0.5*(r[2]/f)
+        sigma_y = 0.5*(r[3]/f)
+        guess = [amp, x0, y0, sigma_x, sigma_y, 0, z0]
+        
+        # run the zoomed-in fit and compute its relative error
+        fine_fit, best = hp.iterfit(hp.residual,guess,x_c,y_c,width,height,coarse, 1)
+        best[1] = best[1] + r[0]
+        best[2] = best[2] + r[1]
+        error = (coarse - fine_fit) / coarse
+        area = (r[2] * r[3]) / (f**2)
+        int_error = (np.sum((error)**2) / area) * 1000
+        print("Integrated error: " + str(round(int_error, 2)))
+    
+    # generate final-fit transmission data; compute relative error
+    params0 = hp.list2params(best)
+    fit_data = 1 - hp.gaussian(params0, x,y)
+    final_error = (transmission - fit_data) / transmission
+
+    stop = time.clock()
+    print("2D fitting took " + str(round(stop - start, 2)) + " seconds")
+    print(" ")
+
+    ##################################################
+    ######## 3: GAUSSIAN FITTING (1D SLICES) #########
+    ##################################################
+
+    print("3: GAUSSIAN FITTING (1D SLICES)")
+    print("-------------------------------")
+    start = time.clock()
+
+    # define the best-fit axes
+    x_val = np.linspace(-2*width, 2*width, 4*width)
+    y_val = np.linspace(-2*height, 2*height, 4*height)
+    (x_hor, y_hor, x_ver, y_ver) = hp.lines(x_val, best)
+
+    # collect (Gaussian) data along these axes
+    print("Collecting 1D data")
+    (x_axis, horizontal) = hp.collect_data(transmission, x_hor, y_hor, 'x')
+    (y_axis, vertical) = hp.collect_data(transmission, x_ver, y_ver, 'y')
+
+    # perform a 1D Gaussian fit on each data set:
+    # for the 1D fits, take the guess [A, x0/y0, sigma_x/sigma_y, z0]
+    guess_h = np.array([best[0], best[1], best[3], best[6]])
+    guess_v = np.array([best[0], best[2], best[4], best[6]])
+
+    # perform the horizontal and vertical 1D fits
+    fit_h, param_h = hp.fit_1d(hp.residual_1d, guess_h, x_axis, horizontal)
+    fit_v, param_v = hp.fit_1d(hp.residual_1d, guess_v, y_axis, vertical)
+
+    stop = time.clock()
+    print("1D fitting took " + str(round(stop - start, 2)) + " seconds")
+    print(" ")
+
+    ##################################################
+    ########## 4: PHYSICAL DENSITY ANALYSIS ##########
+    ##################################################
+
+    print("4: PHYSICAL DENSITY ANALYSIS")
+    print("-------------------------------")
+    start = time.clock()
+
+    # sodium and camera parameters
+    lam = 589.158e-9 # resonant wavelength
+    sigma_0 = (3.0/(2.0*math.pi)) * (lam)**2 # cross-section
+    area = (pixelsize * 1e-3)**2 # pixel area in SI units
+    
+    density = -np.log(transmission)
+    if mode == 'manual':
+        density = -np.log(zoomed)
+    atom_num = (area/sigma_0) * np.sum(density)
+    print("Atom number: " + str(np.round(atom_num/1e6, 2)) + " million")
+
+    stop = time.clock()
+    print("Doing physics took " + str(round(stop - start, 2)) + " seconds")
+    print(" ")
+
+    ##################################################
+    ######### 5: GRAPHS, PLOTS, AND PICTURES #########
+    ##################################################
+
+    print("5: GRAPHS, PLOTS, AND PICTURES")
+    print("-------------------------------")
+    start = time.clock()
+
+    # preliminary plots: 3 images and transmission
+    """
+    plt.figure(1)
+    plt.imshow(data, cmap = 'inferno', extent = pixels)
+    plt.colorbar()
+    plt.figure(2)
+    plt.imshow(beam, cmap = 'inferno', extent = pixels)
+    plt.colorbar()
+    plt.figure(3)
+    plt.imshow(dark, cmap = 'inferno', extent = pixels)
+    plt.colorbar()
+    plt.figure(4)
+    plt.imshow(data - dark, cmap = 'inferno', extent = pixels)
+    plt.colorbar()
+    plt.figure(5)
+    plt.imshow(beam - dark, cmap = 'inferno', extent = pixels)
+    plt.colorbar()
+    plt.figure(6)
+    plt.imshow(transmission, cmap = 'inferno', extent = pixels)
+    plt.colorbar()
+    plt.show()
+    """
+
+    # coarse and fine fits, and relative errors
+    """
+    plt.figure(1)
+    plt.imshow(final_error, cmap = 'inferno', extent = pixels)
+    plt.colorbar()
+    plt.show()
+    """
+    
+    stop = time.clock()
+    print("Plotting graphs took " + str(round(stop - start, 2)) + " seconds")
+    print(" ")
+
+    ##################################################
+    ######### 6: SAVE AND DISPLAY FINAL PLOT #########
+    ##################################################
+
+    # create figure: 3x3 grid (boxes labeled 0-8) containing:
+    # - title (1);
+    # - best-fit parameter outputs (8)
+    # - horizontal (4) and vertical (6) plots
+    # - transmission plot, in color (7)
+    # - empty boxes: 0, 2, 3, 5
+    
+    print("6: SAVE AND DISPLAY FINAL PLOT")
+    print("-------------------------------")
+    start = time.clock()
+
+    print("Painting Gaussian fits in oil")
+    norm_min = -0.1
+    norm_max = 1
+    norm = plt.Normalize(norm_min, norm_max)
+    
+    fig = plt.figure(1)
+    wr = [0.5, 5, 0.5]
+    hr = [0.001, 0.5, 4]
+    gs = gridspec.GridSpec(3, 3, width_ratios = wr, height_ratios = hr)
+    font = {'size'   : 6}
+    plt.rc('font', **font)
+
+    # title and pixel scale
+    ax1 = plt.subplot(gs[1])
+    plt.axis('off')
+    title = 'Gaussian Fit to Absorption Data: shot ' + str(shot)
+    plt.text(0.1, 1, title, fontsize = 10)
+
+    # best-fit parameters: convert to text
+    A = str(np.round(best[0], 2))
+    x_0 = str(np.round(pixelsize * best[1], 3))
+    y_0 = str(np.round(pixelsize * best[2], 3))
+    w_x = str(np.round(2 * pixelsize * best[3], 3))
+    w_y = str(np.round(2 * pixelsize * best[4], 3))
+    # w_x = str(np.round(2 * pixelsize * param_h[2], 2))
+    # w_y = str(np.round(2 * pixelsize * param_v[2], 2))
+    (theta, z_0) = (str(np.round(best[5], 2)), str(np.round(best[6], 2)))
+
+    text1 = 'Best-fit parameters:'
+    text2 = 'A = ' + A
+    text3 = 'x_0 = ' + x_0
+    text4 = 'y_0 = ' + y_0
+    text5 = 'w_x = '+ w_x
+    text6 = 'w_y = '+ w_y
+    # text7 = 'theta = '+ theta + ' rad'
+    text8 = 'N = ' + str(np.round(atom_num/1000000.0, 2)) + ' million'
+
+    # best-fit parameters: display
+    ax8 = plt.subplot(gs[8])
+    plt.axis('off')
+    plt.text(0, 0.8, text1)
+    plt.text(0, 0.7, text2)
+    plt.text(0, 0.6, text3)
+    plt.text(0, 0.5, text4)
+    plt.text(0, 0.4, text5)
+    plt.text(0, 0.3, text6)
+    plt.text(0, 0.2, text8)
+
+    # horizontal and vertical 1D fits
+    ax4 = plt.subplot(gs[4])
+    plt.plot(x_axis, 1 - horizontal, 'ko', markersize = 1)
+    plt.plot(x_axis, 1 - fit_h, 'r', linewidth = 0.5)
+    plt.xlim(0, width)
+    plt.ylim(norm_min, norm_max)
+    plt.gca().axes.get_xaxis().set_visible(False)
+
+    ax6 = plt.subplot(gs[6])
+    plt.plot(1 - vertical, y_axis, 'ko', markersize = 1)
+    plt.plot(1 - fit_v, y_axis, 'r', linewidth = 0.5)
+    plt.xlim(norm_max, norm_min)
+    plt.ylim(height, 0)
+    plt.gca().axes.get_yaxis().set_visible(False)
+
+    # transmission plot with axis lines and zoom box
+    ax7 = plt.subplot(gs[7])
+    plt.imshow(1 - transmission, cmap='inferno', norm=norm, extent=pixels)
+    plt.plot(pixelsize*x_hor, pixelsize*y_hor, color = 'g', linewidth = 0.5)
+    plt.plot(pixelsize*x_ver, pixelsize*y_ver, color = 'g', linewidth = 0.5)
+
+    plt.xlim(pixels[0], pixels[1])
+    plt.ylim(pixels[2], pixels[3])
+
+    # save best-fit parameters and image to files
+    save_path = '..'
+    now = time.strftime("%Y%m%d-%H%M%S")
+
+    pic_path = save_path + '/Analysis Results/'+ now + '.png'
+    txt_path = save_path + '/Analysis Results/diary.txt'
+
+    print("Saving image and writing to diary")
+    diary = open(txt_path, "a+")
+    diary_text = (now, np.round(best, 2), np.round(int_error, 2))
+    diary.write("Time: %s. Fit: %s. Error: %s. \n" % diary_text)
+    diary.close()
+    plt.savefig(pic_path, dpi = 500)
+
+    plt.ion()
+    plt.pause(.1)
+    plt.draw()
+    plt.show(block=False)
+
+
+    stop = time.clock()
+    final = time.clock()
+    print("Saving results took " + str(round(stop - start, 2)) + " seconds")
+    print("Total runtime: " + str(round(final - initial, 2)) + " seconds")
+    print("Ready for the next shot!")
+    print(" ")
+
+    shot += 1
